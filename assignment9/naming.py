13f9e203c7629a162fd9b87b2989a9357e8b7932
Rajvir2057 <rajvirkaur2057@gmail.com>
2024-02-14 12:16:06 +0300

    edit

---
+++ assignment9/naming.py
@@ -0,0 +1,23 @@
+#this is different 
+# errors occur when we place a name beggining with a number, - , and space 
+#Eg
+
+2myvar = "john" 
+
+my-var= 2
+
+my var = 90             
+
+print (2myvar)
+print(my-var)
+print(my var)
+
+#how they can be; 
+
+myvar= 1
+my_var= 2
+_my_var= 3
+myVar= 4
+MYVAR= 5
+myvar2= "john"
+print(myvar)
